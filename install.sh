3e605f0026149f97a6a3301e1a2377662feb7d85
wangyuehong <wangyuehong@gmail.com>
2013-12-26 22:14:28 +0900

    refactoring

--- install.sh
+++ install.sh
@@ -1,56 +1,28 @@
 #!/bin/sh
 
-cwd=$(pwd)
-
-if [ ! -f $cwd/_zshrc ]; then
-    echo "run this sh in git folder"
-    exit 1;
-fi
-
-if [ ! -d ~/.zz ]; then
-    git clone https://github.com/rupa/z.git ~/.zz
-    if [ ! -f ~/.z ]; then
-        touch ~/.z
+function clone_or_pull {
+    if [ ! -d $3 ]; then
+        echo "\nclone $1"
+        git clone $2 $3
+    else
+        echo "\nupdate $1"
+        cd $3 && git pull
     fi
-else
-    echo 'update z'
-    cd ~/.zz && git pull
+}
+
+if [ ! -f ~/.z ]; then
+    touch ~/.z
 fi
 
-if [ ! -d ~/.oh-my-zsh ]; then
-    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-else
-    echo 'update oh-my-zsh'
-    cd ~/.oh-my-zsh && git pull
-fi
+clone_or_pull z https://github.com/rupa/z.git ~/.zz
+clone_or_pull oh-my-zsh https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+clone_or_pull rbenv https://github.com/sstephenson/rbenv.git ~/.rbenv
+clone_or_pull ruby-build https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+clone_or_pull rbenv-gemset https://github.com/jamis/rbenv-gemset.git  ~/.rbenv/plugins/rbenv-gemset
+clone_or_pull rbenv-gem-rehash https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
 
-if [ ! -d ~/.rbenv ]; then
-    git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-else
-    echo 'update rbenv'
-    cd ~/.rbenv && git pull
-fi
+cwd=`dirname $0`
 
-if [ ! -d ~/.rbenv/plugins/ruby-build ]; then
-    git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-else
-    echo 'update rbenv ruby-build'
-    cd ~/.rbenv/plugins/ruby-build && git pull
-fi
-
-if [ ! -d ~/.rbenv/plugins/rbenv-gemset ]; then
-    git clone git://github.com/jamis/rbenv-gemset.git  ~/.rbenv/plugins/rbenv-gemset
-else
-    echo 'update rbenv rbenv-gemset'
-    cd ~/.rbenv/plugins/rbenv-gemset && git pull
-fi
-
-if [ ! -d ~/.rbenv/plugins/rbenv-gem-rehash ]; then
-    git clone git://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
-else
-    echo 'update rbenv rbenv-rehash'
-    cd ~/.rbenv/plugins/rbenv-gem-rehash && git pull
-fi
 ln -sf $cwd/_tmux.conf ~/.tmux.conf
 ln -sf $cwd/_zshrc ~/.zshrc
 ln -sf $cwd/_zshrc_alias ~/.zshrc_alias
